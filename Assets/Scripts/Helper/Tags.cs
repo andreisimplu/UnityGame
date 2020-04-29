e46cab48329ca4b77c0cb9a09d37f0740840c76d
andreisimplu <andrewwarp123@gmail.com>
2020-04-30 02:44:48 +0300

    Mouse Control, Mouse Look, Player Sprint and Crouch, Footsteps Sound

--- Assets/Scripts/Helper/Tags.cs
+++ Assets/Scripts/Helper/Tags.cs
@@ -8,3 +8,11 @@ public class Axis
     public const string VERTICAL = "Vertical";
 
 }
+
+
+public class MouseAxis
+{
+    public const string MOUSE_X = "Mouse X";
+
+    public const string MOUSE_Y = "Mouse Y";
+}
