08d3eb5bb337d2070f3e836bb83167595285b277
andreisimplu <andrewwarp123@gmail.com>
2020-05-01 02:19:26 +0300

    Player Attack, Zoom In, Zoom Out Added

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private WeaponManager weapon_Manger;
+    public float fireRate = 15f;
+
+    private float nextTimeToFire;
+    public float damage = 20f;
+
+    private Animator zoomCameraAnim;
+    private bool zoomed;
+
+    private Camera mainCam;
+
+    private GameObject crosshair;
+
+    private bool is_Aiming;
+
+    private void Awake()
+    {
+        weapon_Manger = GetComponent<WeaponManager>();
+
+        zoomCameraAnim = transform.Find(Tags.LOOK_ROOT).transform.Find(Tags.ZOOM_CAMERA).GetComponent<Animator>();
+
+        crosshair = GameObject.FindWithTag(Tags.CROSSHAIR);
+    }
+
+    void Start()    
+    {
+        
+    }
+
+    void Update()
+    {
+        WeaponShoot();
+        ZoomInAndOut();
+    }
+
+    void WeaponShoot()
+    {
+        if(weapon_Manger.GetCurrentSelectedWeapon().fireType == WeaponFireType.MULTIPLE)
+        {
+            if(Input.GetMouseButton(0) && Time.time > nextTimeToFire)
+            {
+                nextTimeToFire = Time.time + 1f / fireRate;
+
+                weapon_Manger.GetCurrentSelectedWeapon().ShootAnimation();            
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if(weapon_Manger.GetCurrentSelectedWeapon().tag == Tags.AXE_TAG)
+                {
+                    weapon_Manger.GetCurrentSelectedWeapon().ShootAnimation();
+                }
+
+                if(weapon_Manger.GetCurrentSelectedWeapon().bulletType == WeaponBulletType.BULLET)
+                {
+                    weapon_Manger.GetCurrentSelectedWeapon().ShootAnimation();
+                }
+                else
+                {
+                    if (is_Aiming)
+                    {
+                        weapon_Manger.GetCurrentSelectedWeapon().ShootAnimation();
+
+                        if(weapon_Manger.GetCurrentSelectedWeapon().bulletType == WeaponBulletType.ARROW)
+                        {
+
+                        }
+                        else if (weapon_Manger.GetCurrentSelectedWeapon().bulletType == WeaponBulletType.SPEAR)
+                        {
+
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+    void ZoomInAndOut() 
+    { 
+        if(weapon_Manger.GetCurrentSelectedWeapon().weapon_Aim == WeaponAim.AIM)
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                zoomCameraAnim.Play(AnimationTags.ZOOM_IN_ANIM);
+
+                crosshair.SetActive(false);
+            }
+        }
+            if (Input.GetMouseButtonUp(1))
+            {
+            zoomCameraAnim.Play(AnimationTags.ZOOM_OUT_ANIM);
+
+            crosshair.SetActive(true);
+            }
+
+            if(weapon_Manger.GetCurrentSelectedWeapon().weapon_Aim == WeaponAim.SELF_AIM)
+            {
+            if (Input.GetMouseButtonDown(1))
+            {
+                weapon_Manger.GetCurrentSelectedWeapon().Aim(true);
+                is_Aiming = true;
+            }
+
+            }
+            if (Input.GetMouseButtonUp(1))
+            {
+            weapon_Manger.GetCurrentSelectedWeapon().Aim(false);
+            is_Aiming = false;
+            }
+
+    }
+
+
+}
